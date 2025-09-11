d4dd24711d6beda5a7774933b83c249ed9d916c6
Nikunj <nikunjbhartia05@gmail.com>
2025-09-12 00:07:30 +0530

    made functions

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,21 @@
 export const cart=[
 
 ];
+
+export function addToCart(productId){
+        let matchingItem;
+            cart.forEach((cartitem)=>{
+                if(productId===cartitem.productId){
+                    matchingItem=cartitem;
+                }
+            });
+            if (matchingItem){
+                matchingItem.quantity+=1;
+            }
+            else{
+            cart.push({
+                productId: productId,
+                quantity: 1
+            });
+        }
+    }
